7d732f4718c05fbb0da4a4f87ecfcd986eb9b639
tanhanson <144844237+tanhanson@users.noreply.github.com>
2025-09-30 22:56:40 -0400

    eliminer

--- Assets/Script/ControlePompon.cs
+++ Assets/Script/ControlePompon.cs
@@ -20,140 +20,107 @@ public class ControlePompon : MonoBehaviour
 
     int compteur = 0;
 
-
     public TextMeshProUGUI CompteurPoint;
 
-    bool partieTerminee;  // Indique si la partie est terminée  
- 
-    // Start is called before the first frame update
+    bool partieTerminee;  // Indique si la partie est terminÃ©e  
+
     void Start()
     {
-       
     }
 
-    // Update is called once per frame
     void Update()
     {
-
-        if (!partieTerminee)    // Vérifie si la partie n'est pas terminée
-
+        if (!partieTerminee)
         {
-
-
-            if (Input.GetKey(KeyCode.D))    // Se déplacer vers la droite
+            // DÃ©placements horizontaux
+            if (Input.GetKey(KeyCode.D))
             {
                 vitesseX = vitesseXMax;
-                GetComponent<SpriteRenderer>().flipX = false;   // Regarder vers la droite
-
+                GetComponent<SpriteRenderer>().flipX = false;
             }
-            else if (Input.GetKey(KeyCode.A))   // Se déplacer vers la gauche
+            else if (Input.GetKey(KeyCode.A))
             {
                 vitesseX = -vitesseXMax;
-                GetComponent<SpriteRenderer>().flipX = true;    // Regarder vers la gauche
+                GetComponent<SpriteRenderer>().flipX = true;
             }
             else
             {
-                vitesseX = GetComponent<Rigidbody2D>().velocity.x;  // Maintenir la vitesse X actuelle
+                vitesseX = GetComponent<Rigidbody2D>().velocity.x;
             }
 
-
-
-            if (Input.GetKey(KeyCode.W) && Physics2D.OverlapCircle(transform.position, 1f))   // Gère le saut   Vérifie si on est au sol
+            // Saut
+            if (Input.GetKey(KeyCode.W) && Physics2D.OverlapCircle(transform.position, 1f))
             {
-
-                vitesseY = vitesseSaut; // Appliquer la force de saut
-                GetComponent<Animator>().SetBool("saut", true); // Déclencher l'animation de saut
-
+                vitesseY = vitesseSaut;
+                GetComponent<Animator>().SetBool("saut", true);
             }
             else
             {
-                vitesseY = GetComponent<Rigidbody2D>().velocity.y;  // Maintenir la vitesse Y actuelle
+                vitesseY = GetComponent<Rigidbody2D>().velocity.y;
             }
 
-            if (vitesseX > 0.1f || vitesseX < -0.1f)     // Met à jour l'animation en fonction du mouvement
-            {
+            // Animation marche
+            GetComponent<Animator>().SetBool("marche", Mathf.Abs(vitesseX) > 0.1f);
 
-                GetComponent<Animator>().SetBool("marche", true);   // Déclencher l'animation de marche
-            }
-            else
-            {
-
-                GetComponent<Animator>().SetBool("marche", false);  // Déclencher l'animation d'inactivité
-            }
-            GetComponent<Rigidbody2D>().velocity = new Vector2(vitesseX, vitesseY); // Met à jour la vitesse du Rigidbody avec les vitesses horizontale et verticale actuelles
+            // Appliquer les vitesses au Rigidbody
+            GetComponent<Rigidbody2D>().velocity = new Vector2(vitesseX, vitesseY);
         }
     }
+
     private void OnCollisionExit2D(Collision2D collision)
     {
-        {
-            transform.parent = null;
-        }
+        transform.parent = null;
     }
+
     void OnCollisionEnter2D(Collision2D collision)
     {
+        // ArrÃªter l'animation marche si collision avec aigle
         if (collision.gameObject.name == "aigle")
-        {
             GetComponent<Animator>().SetBool("marche", false);
-        }
 
+        // RÃ©initialiser l'animation de saut si on touche le sol
         if (Physics2D.OverlapCircle(transform.position, 1f))
+            GetComponent<Animator>().SetBool("saut", false);
+
+        // Gestion des ennemis
+        if (collision.gameObject.tag == "ennemi")
         {
-            GetComponent<Animator>().SetBool("saut", false);     // Réinitialiser l'animation de saut
+            // Si l'Ã©cureuil est en train de descendre (saut)
+            if (GetComponent<Rigidbody2D>().velocity.y < 0)
+            {
+                Destroy(collision.gameObject); // Tue l'ennemi
+            }
+            else
+            {
+                // Sinon, l'Ã©cureuil prend des dÃ©gÃ¢ts
+                compteurVie += 1;
+            }
         }
 
-        // Gère les collisions avec les ennemis et les objets à collecter
-        if (collision.gameObject.name == "fourmi" || collision.gameObject.name == "crocodile" || collision.gameObject.name == "chien" || collision.gameObject.name == "batVol" || collision.gameObject.name == "batRepos" || collision.gameObject.name == "lapin" || collision.gameObject.name == "dino" || collision.gameObject.name == "petitOurs" || collision.gameObject.name == "grosOurs" || collision.gameObject.name == "vulture")  // Collision avec un ennemi
-
+        // Collecte des noisettes
+        if (collision.gameObject.name == "noisette")
         {
-            compteurVie += 1;
-            
-
-        }
-        
-
-        if (compteurVie == 1)
-        {
-            Destroy(GameObject.Find("coeur1"));
-            
-        }
-        if (compteurVie == 2)
-        {
-            Destroy(GameObject.Find("coeur2"));
-            
-        }
-        if (compteurVie == 3)
-        {
-            Destroy(GameObject.Find("coeur3"));
-            GetComponent<Animator>().SetBool("mort", true); // Déclencher l'animation de mort
-            GetComponent<AudioSource>().PlayOneShot(sonMort);
-
-            partieTerminee = true;  // Définir le drapeau de fin de partie
-
-            Invoke("recommencer", 2f);   // Redémarrer le jeu après un délai
-        }
-
-        
-
-
-
-
-    if (collision.gameObject.name == "noisette" )    // Collecte d'une noisette
-
-        {
-            Destroy(collision.gameObject);  // Détruire l'objet noisette
+            Destroy(collision.gameObject);
             compteur += 1;
             CompteurPoint.text = "Noix : " + compteur.ToString();
             GetComponent<AudioSource>().PlayOneShot(sonDegat);
         }
-       
 
+        // Gestion des plateformes
         if (collision.gameObject.tag == "plateforme")
-        {
             transform.parent = collision.gameObject.transform;
+
+        // Gestion des vies
+        if (compteurVie == 1) Destroy(GameObject.Find("coeur1"));
+        if (compteurVie == 2) Destroy(GameObject.Find("coeur2"));
+        if (compteurVie == 3)
+        {
+            Destroy(GameObject.Find("coeur3"));
+            GetComponent<Animator>().SetBool("mort", true);
+            GetComponent<AudioSource>().PlayOneShot(sonMort);
+            partieTerminee = true;
+            Invoke("recommencer", 2f);
         }
-    
-      
-        
     }
 
     void OnTriggerEnter2D(Collider2D infoCollider)
@@ -173,7 +140,6 @@ public class ControlePompon : MonoBehaviour
 
     void recommencer()
     {
-        SceneManager.LoadScene("FinalMort"); // Charger la scène "IntroJeu" pour redémarrer le jeu
+        SceneManager.LoadScene("FinalMort"); // RedÃ©marre le jeu
     }
-  
 }
